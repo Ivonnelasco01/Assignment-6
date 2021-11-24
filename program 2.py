c323bd6cb0925956afc90f44e3537c9d65b1d585
Jaren <ivonnelasco01@gmail.com>
2021-11-25 03:36:00 +0800

    description

--- program 2.py
+++ program 2.py
@@ -1,91 +1,84 @@
-def _q1():
-    q1 = int(input("1 + 2 = "))#3
-    if q1 == 3 or q1 != 3:
-        if q1 == 3:
+print("Addition Quiz")
+score = 0
+
+q1 = int(input("1 + 2 = "))#3
+if q1 == 3 or q1 != 3:
+    if q1 == 3:
+        print("Correct")
+        score+=1
+    else:
+        print("Incorrect")
+
+q2 = int(input("3 + 4 = ")) #7
+if q2 == 7 or q2 != 7:
+    if q2 == 7:
+        print("Correct")
+        score+= 1
+    else:
+        print("Incorrect")
+
+q3 = int(input("5 + 6 = ")) #11
+if q3 == 11 or q3 != 11:
+    if q3 == 11:
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+
+q4 = int(input("7 + 8 = ")) #15
+if q4 == 15 or q4 != 15:
+    if q4 == 15:
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+
+q5 = int(input("10 + 11 = ")) #21
+if q5 == 21 or q5 != 21:
+    if q5 == 7:
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+
+q6 = int(input("12 + 13 = ")) #25
+if q6 == 25 or q2 != 25:
+    if q6 == 25:
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+
+q7 = int(input("14 + 15 = ")) #29
+if q7 == 29 or q7 != 29:
+    if q7 == 29:
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+
+q8 = int(input("16 + 17 = ")) #33
+if q8 == 33 or q8 != 33:
+    if q8 == 7:
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+
+q9 = int(input("18 + 19 = ")) #37
+if q9 == 37 or q9 != 37:
+    if q9 == 37:
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+
+q10 = int(input("20 + 21 = ")) #41
+if q10 == 41 or q2 != 41:
+    if q10 == 41:
             print("Correct")
-        else:
-            print("Incorrect")
+            score += 1
+    else:
+        print("Incorrect")
 
-def _q2():
-    q2 = int(input("3 + 4 = ")) #7
-    if q2 == 7 or q2 != 7:
-        if q2 == 7:
-            print("Correct")
-        else:
-            print("Incorrect")
-
-def _q3():
-    q3 = int(input("5 + 6 = ")) #11
-    if q3 == 11 or q3 != 11:
-        if q3 == 11:
-            print("Correct")
-        else:
-            print("Incorrect")
-
-def _q4():
-    q4 = int(input("7 + 8 = ")) #15
-    if q4 == 15 or q4 != 15:
-        if q4 == 15:
-            print("Correct")
-        else:
-            print("Incorrect")
-
-def _q5():
-    q5 = int(input("10 + 11 = ")) #21
-    if q5 == 21 or q5 != 21:
-        if q5 == 7:
-            print("Correct")
-        else:
-            print("Incorrect")
-
-def _q6():
-    q6 = int(input("12 + 13 = ")) #25
-    if q6 == 25 or q2 != 25:
-        if q6 == 25:
-            print("Correct")
-        else:
-            print("Incorrect")
-
-def _q7():
-    q7 = int(input("14 + 15 = ")) #29
-    if q7 == 29 or q7 != 29:
-        if q7 == 29:
-            print("Correct")
-        else:
-            print("Incorrect")
-
-def _q8():
-    q8 = int(input("16 + 17 = ")) #33
-    if q8 == 33 or q8 != 33:
-        if q8 == 7:
-            print("Correct")
-        else:
-            print("Incorrect")
-
-def _q9():
-    q9 = int(input("18 + 19 = ")) #37
-    if q9 == 37 or q9 != 37:
-        if q9 == 37:
-            print("Correct")
-        else:
-            print("Incorrect")
-
-def _q10():
-    q10 = int(input("20 + 21 = ")) #41
-    if q10 == 41 or q2 != 41:
-        if q10 == 41:
-            print("Correct")
-        else:
-            print("Incorrect")
-
-q1 = _q1()
-q2 = _q2()
-q3 = _q3()
-q4 = _q4()
-q5 = _q5()
-q6 = _q6()
-q7 = _q7()
-q8 = _q8()
-q9 = _q9()
-q10 = _q10()
-
+print("Result: ", score,"/ 10")
