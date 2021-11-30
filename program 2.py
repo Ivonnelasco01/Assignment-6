92c670ee0891c5fd9b6b4881ca3fa7c4526d9531
Jaren <ivonnelasco01@gmail.com>
2021-11-30 20:10:52 +0800

    description

--- program 2.py
+++ program 2.py
@@ -1,84 +1,25 @@
-print("Addition Quiz")
+import random
+questions = {}
 score = 0
+print("Math Quiz")
 
-q1 = int(input("1 + 2 = "))#3
-if q1 == 3 or q1 != 3:
-    if q1 == 3:
-        print("Correct")
+def quiz(n):
+    for i in range(10):
+        int_a = random.randint(0,99)
+        int_b = random.randint(0,99)
+        operator = ("+")
+        question = str(int_a)+" "+(operator)+" "+str(int_b)
+        answer = eval(question)
+        question += " = "
+
+        questions.update({question: str(answer)})
+quiz(10)
+for q in questions.keys():
+    user_answer = input(q)
+    if questions.get(q) == str(user_answer):
         score+=1
-    else:
-        print("Incorrect")
-
-q2 = int(input("3 + 4 = ")) #7
-if q2 == 7 or q2 != 7:
-    if q2 == 7:
         print("Correct")
-        score+= 1
     else:
         print("Incorrect")
 
-q3 = int(input("5 + 6 = ")) #11
-if q3 == 11 or q3 != 11:
-    if q3 == 11:
-        print("Correct")
-        score += 1
-    else:
-        print("Incorrect")
-
-q4 = int(input("7 + 8 = ")) #15
-if q4 == 15 or q4 != 15:
-    if q4 == 15:
-        print("Correct")
-        score += 1
-    else:
-        print("Incorrect")
-
-q5 = int(input("10 + 11 = ")) #21
-if q5 == 21 or q5 != 21:
-    if q5 == 7:
-        print("Correct")
-        score += 1
-    else:
-        print("Incorrect")
-
-q6 = int(input("12 + 13 = ")) #25
-if q6 == 25 or q2 != 25:
-    if q6 == 25:
-        print("Correct")
-        score += 1
-    else:
-        print("Incorrect")
-
-q7 = int(input("14 + 15 = ")) #29
-if q7 == 29 or q7 != 29:
-    if q7 == 29:
-        print("Correct")
-        score += 1
-    else:
-        print("Incorrect")
-
-q8 = int(input("16 + 17 = ")) #33
-if q8 == 33 or q8 != 33:
-    if q8 == 7:
-        print("Correct")
-        score += 1
-    else:
-        print("Incorrect")
-
-q9 = int(input("18 + 19 = ")) #37
-if q9 == 37 or q9 != 37:
-    if q9 == 37:
-        print("Correct")
-        score += 1
-    else:
-        print("Incorrect")
-
-q10 = int(input("20 + 21 = ")) #41
-if q10 == 41 or q2 != 41:
-    if q10 == 41:
-            print("Correct")
-            score += 1
-    else:
-        print("Incorrect")
-
-print("Result: ", score,"/ 10")
+print("Result: "+str(score)+"/10")
